dc65695f1df965a3f7ea00d55ca61f1b1f9a9581
Mingo Emmanuel Ngaounui <mingoemmanuel06@gmail.com>
2026-03-02 18:57:14 +0100

    CountSort1.py

---
+++ CountingSort1.py
@@ -0,0 +1,21 @@
+def countingSort(arr):
+    freq = [0] * 100
+    
+    for num in arr:
+        freq[num] += 1
+        
+    return freq
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
