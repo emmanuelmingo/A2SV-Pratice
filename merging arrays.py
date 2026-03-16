58521712cdf82fecdfa470541ff14d696b1bae80
Mingo Emmanuel Ngaounui <mingoemmanuel06@gmail.com>
2026-03-16 08:57:01 +0100

    merging arrays.py

---
+++ merging arrays.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+i = j = 0
+merged = []
+
+while i < n and j < m:
+    if a[i] <= b[j]:
+        merged.append(a[i])
+        i += 1
+    else:
+        merged.append(b[j])
+        j += 1
+
+while i < n:
+    merged.append(a[i])
+    i += 1
+
+while j < m:
+    merged.append(b[j])
+    j += 1
+
+print(*merged)
